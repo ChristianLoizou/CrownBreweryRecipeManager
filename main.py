10fa730d3594dc7e8138b2e1c8559c5f9d12aeea
Christian Loizou <loizouchristian@gmail.com>
2020-09-12 13:13:40 +0100

    Created dynamic instantiation method for all widget types in Application

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import *
 from tkinter.ttk import Separator
 
 class Beer:
-
+    ''' Beer object. Stores all data about custom beers, including name, recipe, ABV, gravity, etc...'''
     def __init__(self, name, jsondata):
         self.name, self.type = name, jsondata["type"]
         self.abv, self.gravity = jsondata["ABV"], jsondata["gravity"]
@@ -31,10 +31,10 @@ class Beer:
             t, d = datapairs[r-1]
             Label(popup, text=t.capitalize()).grid(row=r+1, column=0)
             Label(popup, text=d).grid(row=r+1, column=1)
-        
+
 
 class Application(Tk):
-
+    ''' Application object. Blueprint for the window shown to user, with custom methods to allow for easier adding of widgets'''
     def __init__(self, /, *, title, size="800x600+400+200", jsonpath="data/beers.json", iconpath="assets/icon.ico"):
         self.app = Tk()
         self.title, self.iconpath = title, iconpath
@@ -58,57 +58,33 @@ class Application(Tk):
         self.items[frame_name] = frame
         return frame
 
-    def gridFrame(self, master, frame_name, *args, row, column, gkws=None, **kwargs):
+    def gridWidget(self, master, widget_type, widget_name, *args, row, column, gkws=None, **kwargs):
         if row > self.rows: self.rows = row
         if column > self.cols: self.cols = column
-        frame = Frame(master, kwargs)
-        if gkws: frame.grid(row=row, column=column, **gkws)
-        else: frame.grid(row=row, column=column)
-        self.items[frame_name] = frame
-        return frame
-
-    def gridLabel(self, master, label_name, *args, row, column, gkws=None, **kwargs):
-        if row > self.rows: self.rows = row
-        if column > self.cols: self.cols = column
-        label = Label(master, kwargs)
-        if gkws: label.grid(row=row, column=column, **gkws)
-        else: label.grid(row=row, column=column)
-        self.items[label_name] = label
-        return label
-
-    def gridButton(self, master, button_name, *args, row, column, gkws=None, **kwargs):
-        if row > self.rows: self.rows = row
-        if column > self.cols: self.cols = column
-        button = Button(master, kwargs)
-        if gkws: button.grid(row=row, column=column, **gkws)
-        else: button.grid(row=row, column=column)
-        self.items[button_name] = button
-        return button
-
-    def gridEntry(self, master, entry_name, *args, row, column, gkws=None, **kwargs):
-        if row > self.rows: self.rows = row
-        if column > self.cols: self.cols = column
-        entry = Entry(master, kwargs)
-        if gkws: entry.grid(row=row, column=column, **gkws)
-        else: entry.grid(row=row, column=column)
-        self.items[entry_name] = entry
-        return entry
+        widget = widget_type(master, kwargs)
+        if gkws: widget.grid(row=row, column=column, **gkws)
+        else: widget.grid(row=row, column=column)
+        self.items[widget_name] = widget
+        return widget
 
 def loadBeers(path="data/beers.json"):
+    ''' Loads beer data from JSON file passed as arg '''
     with open(path, "r") as beerfile:
         beerdata = json.load(beerfile)
     return [Beer(k, v) for (k,v) in beerdata.items()]
 
 def saveBeers(path, beers):
+    ''' Saves beer data to JSON file passed as arg '''
     with open(path, "w") as beerfile:
         json.dump(beers, beerfile, indent=2)
 
 def setupWindow():
+    ''' Sets up GUI with widgets '''
     # Create basic window layout
     root = Application(title="Crown Brewery Recipe Manager")
-    titleframe = root.gridFrame(root.app, "frame_titleframe", row=0, column=0, highlightbackground="black", highlightthickness=1,
+    titleframe = root.gridWidget(root.app, Frame, "frame_titleframe", row=0, column=0, highlightbackground="black", highlightthickness=1,
         height=65, gkws={"sticky":"new", "columnspan":3})
-    bodyframe = root.gridFrame(root.app, "frame_bodyframe", row=1, column=0, highlightthickness=1,
+    bodyframe = root.gridWidget(root.app, Frame, "frame_bodyframe", row=1, column=0, highlightthickness=1,
         gkws={"sticky":"nsew", "columnspan":3})
     createframe = root.packLabelledFrame(bodyframe, "frame_createframe", text="Create New Recipe",
         highlightbackground="black", highlightthickness=1,
@@ -122,25 +98,25 @@ def setupWindow():
     for i in range(root.cols): root.app.grid_columnconfigure(i, weight=1)
 
     # Set up the "title" frame
-    root.gridLabel(titleframe, "label_title", row=0, column=0, text="Recipe Manager", font=("Helvetica", 18, "bold"),
+    root.gridWidget(titleframe, Label, "label_title", row=0, column=0, text="Recipe Manager", font=("Helvetica", 18, "bold"),
         gkws={"sticky":"nsew"})
-    root.gridLabel(titleframe, "label_subtitle", row=1, column=0, text="by Christian A Loizou")
+    root.gridWidget(titleframe, Label, "label_subtitle", row=1, column=0, text="by Christian A Loizou")
 
     # Set up the "create" frame
-    root.gridLabel(createframe, "label_entrytitle", row=0, column=0, text="Enter beer name: ")
-    name = root.gridEntry(createframe, "entry_beername", row=0, column=1)
-    root.gridLabel(createframe, "label_entrytitle", row=0, column=2, text="Enter beer type: ")
-    type = root.gridEntry(createframe, "entry_beertype", row=0, column=3)
+    root.gridWidget(createframe, Label, "label_entrytitle", row=0, column=0, text="Enter beer name: ")
+    name = root.gridWidget(createframe, Entry, "entry_beername", row=0, column=1)
+    root.gridWidget(createframe, Label, "label_entrytitle", row=0, column=2, text="Enter beer type: ")
+    type = root.gridWidget(createframe, Entry, "entry_beertype", row=0, column=3)
 
     # Set up the "view" frame
     for beernum, beer in enumerate(root.beers):
         buttonname = "button_"+beer._getformattedname()
-        root.gridButton(viewframe, buttonname, row=1, column=beernum, text=beer.name, width=10, height=10,
+        root.gridWidget(viewframe, Button, buttonname, row=1, column=beernum, text=beer.name, width=10, height=10,
             command=beer.displayInformation)
     return root
 
 
 if __name__ == "__main__":
     application = setupWindow()
-    # print("\n", application, "\n")
+    # print(f"\n{application}\n")
     application.app.mainloop()
